ebedf7bf782e9fb258dbf8464529c5929f9f39c1
Don Gumayagay <dngumayagay@gmail.com>
2023-02-13 16:36:06 +0800

    display todos in realtime

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,37 @@
         onAuthStateChanged,
         signInWithPopup,
     } from "firebase/auth";
+    import {
+        addDoc,
+        collection,
+        doc,
+        onSnapshot,
+        query,
+        where,
+    } from "firebase/firestore";
     import { onMount } from "svelte";
 
+    const todo_collection = collection(db, "todos");
+
     let user;
+    let todo_text = "";
+    let list_of_todos = [];
 
     onMount(() => {
         onAuthStateChanged(auth, (_user) => {
             user = _user;
+            if (user) {
+                const user_todos_query = query(
+                    todo_collection,
+                    where("owner", "==", user.uid)
+                );
+                onSnapshot(user_todos_query, (querySnapshot) => {
+                    list_of_todos = querySnapshot.docs.map((item) => ({
+                        id: item.id,
+                        ...item.data(),
+                    }));
+                });
+            }
         });
     });
 </script>
@@ -27,5 +51,21 @@
         Login with Google
     </button>
 {:else}
-    todo
+    <input type="text" placeholder="todo text" bind:value={todo_text} />
+    <button
+        on:click={async () => {
+            await addDoc(todo_collection, { owner: user.uid, todo_text });
+            todo_text = "";
+        }}
+    >
+        add todo
+    </button>
+
+    <ul>
+        {#each list_of_todos as todo_item}
+            <li>
+                <input type="text" value={todo_item.todo_text} />
+            </li>
+        {/each}
+    </ul>
 {/if}
